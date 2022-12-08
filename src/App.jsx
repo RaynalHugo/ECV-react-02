27b94d4ae384acc7e48010bfc3c7035881b359ca
Lucas-Debiais <lucas.debiais11@gmail.com>
2022-12-08 12:07:12 +0100

    Feat: add player

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,63 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
-import { set } from 'lodash/fp'
+import {set} from 'lodash/fp'
 
 const EMPTY = "Empty";
 const CROSS = "Cross";
 const CIRCLE = "Circle";
 
 function App() {
-  return (
-    <div className="App">
-        <Wrapper />
-    </div>
-  )
+    return (
+        <div className="App">
+            <Wrapper/>
+        </div>
+    )
 }
 
 const defaultState =
-[
-    [EMPTY, EMPTY, EMPTY],
-    [EMPTY, EMPTY, EMPTY],
-    [EMPTY, EMPTY, EMPTY]
-]
+    [
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY]
+    ]
 
 function Wrapper() {
     const [status, setStatus] = useState(defaultState);
-    
+    const [player, setPlayer] = useState(CROSS);
+
     function setCellStatus(row, col, status) {
-        setStatus((previousState) => 
+        setStatus((previousState) =>
             set([row, col], status, previousState)
         )
+        setPlayer(previousPlayer => {
+            if (previousPlayer === CROSS) {
+                return CIRCLE
+            }
+            if (previousPlayer === CIRCLE) {
+                return CROSS
+            }
+        })
     }
 
     return (
         <>
-        <button onClick={() => setCellStatus(0,0, CIRCLE)}>Set Status Test</button>
+            <span>Current player : {player}</span>
             <table className="wrapper">
                 <figcaption>Tic Tac Toe</figcaption>
                 <tr>
-                    <Cell status={status[0][0]} setCellStatus={() => setCellStatus(0, 0, CROSS)}/>
-                    <Cell status={status[0][1]} setCellStatus={() => setCellStatus(0, 1, CROSS)}/>
-                    <Cell status={status[0][2]} setCellStatus={() => setCellStatus(0, 2, CROSS)}/>
+                    <Cell status={status[0][0]} setCellStatus={() => setCellStatus(0, 0, player)}/>
+                    <Cell status={status[0][1]} setCellStatus={() => setCellStatus(0, 1, player)}/>
+                    <Cell status={status[0][2]} setCellStatus={() => setCellStatus(0, 2, player)}/>
                 </tr>
                 <tr>
-                    <Cell status={status[1][0]} setCellStatus={() => setCellStatus(1, 0, CROSS)}/>
-                    <Cell status={status[1][1]} setCellStatus={() => setCellStatus(1, 1, CROSS)}/>
-                    <Cell status={status[1][2]} setCellStatus={() => setCellStatus(1, 2, CROSS)}/>
+                    <Cell status={status[1][0]} setCellStatus={() => setCellStatus(1, 0, player)}/>
+                    <Cell status={status[1][1]} setCellStatus={() => setCellStatus(1, 1, player)}/>
+                    <Cell status={status[1][2]} setCellStatus={() => setCellStatus(1, 2, player)}/>
                 </tr>
                 <tr>
-                    <Cell status={status[2][0]} setCellStatus={() => setCellStatus(2, 0, CROSS)}/>
-                    <Cell status={status[2][1]} setCellStatus={() => setCellStatus(2, 1, CROSS)}/>
-                    <Cell status={status[2][2]} setCellStatus={() => setCellStatus(2, 2, CROSS)}/>
+                    <Cell status={status[2][0]} setCellStatus={() => setCellStatus(2, 0, player)}/>
+                    <Cell status={status[2][1]} setCellStatus={() => setCellStatus(2, 1, player)}/>
+                    <Cell status={status[2][2]} setCellStatus={() => setCellStatus(2, 2, player)}/>
                 </tr>
             </table>
             <pre>{JSON.stringify(status, null, 2)}</pre>
